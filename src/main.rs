f1c92fc0651342f45df3bf347722f22f8f052554
Eamonn Carson <EamonnCarson@users.noreply.github.com>
2022-12-04 17:11:10 -0800

    forgot a bit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod day1;
+mod day01;
 
 fn main() {
-    day1::answer_part_1("res/day1/part1.sample");
-    day1::answer_part_1("res/day1/part1.real");
-    day1::answer_part_2("res/day1/part1.sample");
-    day1::answer_part_2("res/day1/part1.real");
+    day01::answer_part_1("res/day1/part1.sample");
+    day01::answer_part_1("res/day1/part1.real");
+    day01::answer_part_2("res/day1/part1.sample");
+    day01::answer_part_2("res/day1/part1.real");
 }

c4150262c1e2bcebe006e4f6c8a62bb6cc6a4de6
Eamonn Carson <EamonnCarson@users.noreply.github.com>
2022-12-05 00:48:45 -0800

    finished day 5 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
+mod utils;
 
 fn main() {
     /*
@@ -17,9 +19,13 @@ fn main() {
     day03::answer_part_1("res/day03/input.real");
     day03::answer_part_2("res/day03/input.sample");
     day03::answer_part_2("res/day03/input.real");
-    */
     day04::answer_part_1("res/day04/input.sample");
     day04::answer_part_1("res/day04/input.real");
     day04::answer_part_2("res/day04/input.sample");
     day04::answer_part_2("res/day04/input.real");
+    */
+    day05::answer_part_1("res/day05/input.sample");
+    day05::answer_part_1("res/day05/input.real");
+    //day05::answer_part_2("res/day05/input.sample");
+    //day05::answer_part_2("res/day05/input.real");
 }
